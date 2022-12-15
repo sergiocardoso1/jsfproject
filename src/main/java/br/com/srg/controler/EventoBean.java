815d3570d8b98255a5b8def29f7513f3097b32a4
sergiocardoso1 <sergioifbaiano@hotmail.com>
2022-12-15 13:11:45 -0300

    Projeto

---
+++ src/main/java/br/com/srg/controler/EventoBean.java
@@ -0,0 +1,56 @@
+package br.com.srg.controler;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.com.srg.model.Evento;
+
+@Named
+@SessionScoped
+public class EventoBean implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private Evento evento;
+	
+	private List<Evento> eventos = new ArrayList<>();
+	
+	public String adicionarEvento() {
+		eventos.add(evento);
+		System.out.println("Evento " + evento.getNome() + " cadastrado com sucesso!");
+		clear();
+		return "";
+	}
+	
+	public void clear() {
+		this.evento = new Evento();
+	}
+	
+	public EventoBean() {
+		
+	}
+
+	public Evento getEvento() {
+		return evento;
+	}
+
+	public void setEvento(Evento evento) {
+		this.evento = evento;
+	}
+
+	public List<Evento> getEventos() {
+		return eventos;
+	}
+
+	public void setEventos(List<Evento> eventos) {
+		this.eventos = eventos;
+	}
+	
+
+}
